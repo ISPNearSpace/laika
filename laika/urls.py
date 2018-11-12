d4fce5de8db856ef7f90e2c4161f8950f54dfbe6
neworigin <12588861+monchiestevez@users.noreply.github.com>
2018-11-11 23:06:53 -0500

    new predictor design

--- laika/urls.py
+++ laika/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('dashboard.urls')),
@@ -7,3 +9,6 @@ urlpatterns = [
     path('radar/', include('radar.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
